92377285159a564695a315edb2d3346d595eafb9
iamabadcoder <iamabadcoder@gmail.com>
2017-05-20 09:51:21 +0800

    fixbug

--- top_beiyouren_posts.py
+++ top_beiyouren_posts.py
@@ -22,24 +22,26 @@ def login(driver, home_page_url):
     submit_element = None
     try:
         driver.get(home_page_url)
-        id_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id")))
-        passwd_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "pwd")))
-        submit_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "b_login")))
+        id_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "u_login_id")))
+        passwd_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "u_login_passwd")))
+        submit_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "u_login_submit")))
         login_flag = True
     except TimeoutException:
         print 'TimeoutException occur when WebDriverWait in login'
         driver.get(home_page_url)
     if login_flag is False:
         try:
-            id_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id")))
-            passwd_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "pwd")))
-            submit_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "b_login")))
+            id_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "u_login_id")))
+            passwd_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "u_login_passwd")))
+            submit_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "u_login_submit")))
         except TimeoutException:
             print 'TimeoutException occur when WebDriverWait in login'
     else:
-        id_element.send_keys(conf.get("shuimu", "name"))
+        # id_element.clear()
+        id_element.send_keys(conf.get("beiyouren", "name"))
         time.sleep(1)
-        passwd_element.send_keys(conf.get("shuimu", "pwd"))
+        # passwd_element.clear()
+        passwd_element.send_keys(conf.get("beiyouren", "name"))
         time.sleep(1)
         submit_element.click()
         time.sleep(3)
@@ -81,10 +83,10 @@ def login_and_top():
 if __name__ == '__main__':
     conf = ConfigParser.ConfigParser()
     conf.read('account.conf')
-    home_page_url = 'http://www.newsmth.net/nForum/index'
-    file_shuimu_post_urls = './posts_tool/shuimu_post_urls.txt'
+    home_page_url = 'https://bbs.byr.cn/#!default'
+    file_shuimu_post_urls = './posts_tool/beiyouren_post_urls.txt'
     interval_of_top_post = 10  # 循环顶帖的时间间隔
-    cycle_time = 240  # 程序运行周期
+    cycle_time = 300  # 程序运行周期
 
     driver = webdriver.Chrome('./drivers/chromedriver')
     driver.maximize_window()
